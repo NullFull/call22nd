e2092b31d29c5fb7626544012b762e3d1ef2c86a
Juyoung Kim <ingressor531@gmail.com>
2024-02-24 19:28:27 +0900

    chore: 페이지 아이콘 넣기

--- src/components/WhiteButton.tsx
+++ src/components/WhiteButton.tsx
@@ -1,10 +1,25 @@
 import React from 'react';
+import Image from 'next/image';
 import Link from 'next/link';
 
 const WhiteButton = (props: {link: string, message: string}) => {
   return (
     <button className="whiteButton shadow">
       <Link href={props.link}>
+        <Image
+          src="/page.svg"
+          alt="page"
+          width={0}
+          height={0}
+          style={
+            {
+              width: "6vw",
+              height: "auto",
+              verticalAlign: "middle",
+              marginRight: "5px"
+            }
+          }
+        />
         <span className="message">{props.message}</span>
       </Link>
     </button>
