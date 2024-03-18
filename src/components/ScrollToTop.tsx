697d84f75e50c7c0db4a2469313caf5dd53d7267
Dokyun Lim <im.koremp@gmail.com>
2024-03-18 09:32:30 +0900

    feat: add window width to scroll to top

--- src/components/ScrollToTop.tsx
+++ src/components/ScrollToTop.tsx
@@ -1,22 +1,52 @@
-import React from 'react';
+'use client'
+
+import React from 'react'
+import { useEffect, useState } from 'react';
+
 import Image from 'next/image';
 import Link from 'next/link';
 
-const ScrollToTop = (props: {link: string}) => {
+function getWindowWidth() {
+  const { innerWidth: width } = window;
+  return {
+    width,
+  };
+}
+
+function useWindowWidth() {
+  const [width, setWidth] = useState(getWindowWidth());
+
+  useEffect(() => {
+    function handleResize() {
+      const width = getWindowWidth();
+      setWidth(width);
+    }
+
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  return width;
+}
+
+const ScrollToTop = (props: { link: string }) => {
+  const width = useWindowWidth();
+
   return (
     <Link href={props.link}>
       <Image
         src="/images/top.svg"
         alt="scrollToTop"
-        width={0}
-        height={0}
+        width={52}
+        height={52}
         style={
           {
-            width: "50px",
             height: "auto",
             position: "fixed",
             bottom: "150px",
-            right: "25px"
+            border: "1px solid #ffffff",
+            borderRadius: "26px",
+            right: `${(Number(width) - 360) / 2 + 25}px`
           }
         }
       />
