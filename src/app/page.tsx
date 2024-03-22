6a237807cd43a838ce10648f30253129154a75d9
Dokyun Claire Lim <im.koremp@gmail.com>
2024-03-23 03:47:31 +0900

    0322 버전에 맞게 텍스트 수정 및 컴포넌트 위치 재조정  (#20)
    * refactor: 0322 버전에 맞게 메인 페이지 텍스트 수정 및 추가
    
    * fix: fix typo and arrange logo position
    
    * refactor: resize purple button
    
    * refactor: add background to main ask component worker
    
    * misc: padding 줄이기
    
    * misc: 폰트가 한국어를 지원하도록 변경(`Noto Sans KR` 사용)
    
    * misc: 컴포넌트 크기 및 위치 미세 조정
    
    * fix: 로고가 투명하게 되는 문제 해결
    
    ---------
    
    Co-authored-by: Juyoung Kim <ingressor531@gmail.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import dynamic from 'next/dynamic';
+import dynamic from "next/dynamic";
 import Image from "next/image";
 
 import "@/app/main.css"
@@ -6,8 +6,8 @@ import PurpleButton from "@/components/PurpleButton";
 import WhiteButton from "@/components/WhiteButton";
 import WhiteButtonTitle from "@/components/WhiteButtonTitle";
 import MiniStar from "@/components/MiniStar";
-import Ask from '@/components/Ask';
-// import ScrollToTop from '@/components/ScrollToTop';
+import Ask from "@/components/Ask";
+// import ScrollToTop from "@/components/ScrollToTop";
 
 const Scroll = dynamic(
   () => {
@@ -21,22 +21,20 @@ export default function Home() {
     <div className="mainWrapper">
       <main className="main">
         <div className="logo">
+          <div className="circle purple shadow">
+            <span className="circleTitle">Call 22</span>
+          </div>
           <span className="year">2024</span>
-          <span className="circle purple shadow">Call 22</span>
         </div>
 
-        <div className="header">
-          <PurpleButton
-            link={"https://drive.google.com/file/d/10_qClL-HXUuJOZtV-m1LvvlWxf6eehw6/view?usp=sharing"}
-            message={"콜22 캠페인 브로슈어 다운로드"}
-          />
-        </div>
-        <div className="header">
-          <PurpleButton
-            link={"#askSection"}
-            message={"서명운동 참여하기"}
-          />
-        </div>
+        <PurpleButton
+          link={"https://drive.google.com/file/d/10_qClL-HXUuJOZtV-m1LvvlWxf6eehw6/view?usp=sharing"}
+          message={"콜22 캠페인 브로슈어 다운로드"}
+        />
+        <PurpleButton
+          link={"#askSection"}
+          message={"서명운동 참여하기"}
+        />
 
         <div className="title">
           <WhiteButtonTitle
@@ -92,65 +90,67 @@ export default function Home() {
         </div>
 
         <div className="message textarea">
-          <p>우리 동네 국회의원<br />후보자들은<br />불평등한 강간죄를<br />바꿔 볼 생각이 있는 걸까?</p>
+          <p>
+            나는 오늘 성평등에<br />
+            투표합니다.
+          </p>
           <MiniStar />
-          <p>21대에 이어<br />22대 국회의원 후보자들에게<br />또다시 질문하러<br />콜22 팀이 돌아왔습니다!</p>
+          <p>
+            강간죄의 구성요건을<br />
+            &apos;동의여부&apos;로 바꾸고,<br />
+            성평등 전담부처<br />
+            강화하는데 동의하십니까?
+          </p>
+
           <MiniStar />
-          <p>저희는 강간죄의 구성요건을<br />피해자의 &apos;동의&apos; 여부로<br />개정할 의사가 있는지<br />궁금해하는 시민들의 질문을<br /><span className="blue">167,398번</span> 발송했고</p>
-          <Image
-            src="/images/law.png"
-            alt="law"
-            width={144}
-            height={108}
-            style={
-              {
-                marginTop: "15px",
-                marginBottom: "15px"
-              }
-            }
-          />
-          <p>당선된 21대 국회의원들 중<br /><span className="blue">206명</span>이 응답해<br />비동의 강간죄를 도입하는 데<br />찬성한다는<br />의견을 보내주셨습니다.</p>
-          <MiniStar />
-          <p>이후 21대 국회에서는<br />총 2건의 개정안이<br />발의되었지만<br />해당 법안들은 무관심 속에<br />본회의까지 도달하지 못했으며,<br />현재 21대 국회의<br />법안 처리 기한이 만료되어<br />기회를 또다시 잃었습니다.</p>
-          <MiniStar />
-          <p>이상한 나라의 대한민국은<br />판사를 잘 만나야<br />자신의 강간 피해를<br />사법적으로 해결할 수 있는<br />나라이기에<br />수많은 피해 사건들이<br />지금 이 순간에도<br />법망을 빠져나가고 있습니다.</p>
-          <div className="buttons">
-            <WhiteButton
-              link={"https://super-mayonnaise-862.notion.site/7b0077e5966c4be79b1745bc5b3366ba"}
-              message={"2022-2023 강간 무죄 판결 보기"}
-            />
+          <div className="speech">
+            <p>
+              윤석열:<br />
+              “구조적인 성차별은 없다.<br />
+              차별은 개인적 문제”<br />
+              <br />
+              권성동:<br />
+              “비동의 간음죄 도입?<br />
+              무고가 늘어날거에요.<br />
+              이래서 여가부 폐지하자는 겁니다.”<br />
+              <br />
+              한동훈:<br />
+              “어려운 면이 있습니다.<br />
+              이 부분에서 억울한 사람이 처벌받게<br />
+              해서는 안되지 않겠습니까?”<br />
+              <br />
+              법무부:<br />
+              “법무부는 소위 ‘비동의간음죄’ 개정<br />
+              계획이 없음을 알려드립니다.”<br />
+              <br />
+              여성가족부:<br />
+              “비동의 간음죄 개정 검토와<br />
+              관련하여 정부는 개정 계획이<br />
+              없음을 알려드린다”<br />
+            </p>
           </div>
-          <div className="buttons">
-            <WhiteButton
-              link={"https://super-mayonnaise-862.notion.site/21-feaa4e5208e0412dafa84bad1c45816e"}
-              message={"폐기 법안 / 성범죄 기사 보러 가기"}
-            />
-          </div>
-          <Image
-            src="/images/qna.png"
-            alt="qna"
-            width={144}
-            height={108}
-            style={
-              {
-                marginTop: "15px",
-                marginBottom: "15px"
-              }
-            }
-          />
-          <p>우리 동네<br />국회의원 후보자들에게<br />강간죄를 개정할 뜻이 있는지<br />물어봐 주세요.</p>
+
+          <p>
+            22대 국회는<br />
+            성평등 퇴보를 막을 수 있습니까?
+          </p>
           <MiniStar />
-          <p>콜22 팀이 여러분의 질문을<br />대신 전해드립니다.</p>
-        </div>
+          <p>
+            현재까지 000개의 질문과 함께<br />
+            하고 있습니다.<br />
+            <br />
+            클릭 한 번으로 &apos;성평등&apos;을 위한
+            발걸음에 함께 해주세요!
+          </p>
+        </div >
 
         <div className="worker" id="askSection">
-          <p className="message subtitle">국회의원 후보에게 질문하기</p>
           <Ask />
         </div>
 
         <div className="footer">
-          <p>Contact Us</p>
-          <p>call22ndworks@gmail.com</p>
+          <p className="contactText">Contact Us</p>
+          <p className="contactEmail">call22ndworks@gmail.com</p>
         </div>
 
         <div className="scroll">
@@ -158,7 +158,7 @@ export default function Home() {
             link={"#main"}
           />
         </div>
-      </main>
-    </div>
+      </main >
+    </div >
   );
 }
