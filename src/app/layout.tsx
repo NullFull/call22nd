8a0e5b9aa1418940743db4383f45ab8ec228df25
Dokyun Lim <im.koremp@gmail.com>
2024-03-22 19:39:32 +0900

    refactor: add title text to meta and og

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,8 +4,10 @@ import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
+const title: string = "제22대 국회에 요구한다"
+
 export const metadata: Metadata = {
-  title: "제22대 국회에 요구한다",
+  title: title,
   description: "제22대 국회의원 후보자들에게 묻습니다",
 };
 
@@ -17,7 +19,7 @@ export default function RootLayout({
   return (
     <html lang="ko">
 
-      <meta property="og:title" content="제22대 국회에게 요구한다" />
+      <meta property="og:title" content={title} />
       <meta property="og:image" content="./opengraph-image.png" />
       <body className={inter.className}>{children}</body>
     </html>
