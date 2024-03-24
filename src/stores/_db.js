21a6dec4ca608320585f64094435e602038ee232
limkhl <limkh.dev@gmail.com>
2024-03-24 14:00:42 +0900

    feat: add 환경변수

--- src/stores/_db.js
+++ src/stores/_db.js
@@ -14,7 +14,7 @@ const getDB = () => {
                 "auth_uri": "https://accounts.google.com/o/oauth2/auth",
                 "token_uri": "https://oauth2.googleapis.com/token",
                 "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-                "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-nqr4j%40call21st-works.iam.gserviceaccount.com"
+                "client_x509_cert_url": process.env.FIREBASE_CLIENT_X509_CERT_URL
             })
         })
     }
