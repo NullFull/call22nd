678bbb3e088f53b63d4324f023491aaa0662d4da
Dokyun Lim <im.koremp@gmail.com>
2024-03-28 19:22:07 +0900

    refactor: update debounce utils

--- src/utils/debounce.js
+++ src/utils/debounce.js
@@ -1,9 +1,12 @@
 export default function debounce(func, timeout = 300) {
   let timer;
   return (...args) => {
+    if (!timer) {
+      func.apply(this, args);
+    }
     clearTimeout(timer);
     timer = setTimeout(() => {
-      func.apply(this, args);
+      timer = undefined;
     }, timeout);
   };
 }
