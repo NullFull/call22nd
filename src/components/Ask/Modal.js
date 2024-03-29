736fdc3e0a5428d9e6954063130eb82fb18fc7fb
Dokyun Lim <im.koremp@gmail.com>
2024-03-29 22:55:05 +0900

    feat: add modal to ask components

---
+++ src/components/Ask/Modal.js
@@ -0,0 +1,10 @@
+const Modal = () => {
+  return (
+    <div className="modal">
+      <div className="modalContent">
+        질문을 보내는 중입니다.<br/>
+        창을 닫지 말아주세요. 
+      </div>
+    </div>
+  )
+}
