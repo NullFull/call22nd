73f27c703052fc907431f011c7022667ce0a315c
limkhl <limkh.dev@gmail.com>
2024-05-23 16:18:44 +0900

    chore: 당선된 후보 목록을 더 먼저 보여주기

--- src/components/Result.tsx
+++ src/components/Result.tsx
@@ -42,24 +42,24 @@ const Result = () => {
 
     return (
         <div>
-            {agrees.length > 0 &&
-                <div>
-                    <h3 className="listTitle">동의한 후보 {agrees.length}명</h3>
-                    <ul className="list">
-                        {agrees.map(response => (
-                            <li key={`agree-${response.id}`} style={{padding: '10px 0'}}>
-                                <Response member={response.candidate}/>
-                            </li>
-                        ))}
-                    </ul>
-                </div>
-            }
             {electedAgrees.length > 0 && 
                 <div>
                     <h3 className="listTitle">찬성한 후보 중 당선된 후보 {electedAgrees.length}명</h3>
                     <ul className="list">
                         {electedAgrees.map(response => (
                             <li key={`elected-${response.id}`} style={{padding: '10px 0'}}>
+                                <Response member={response.candidate}/>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+            }
+            {agrees.length > 0 &&
+                <div>
+                    <h3 className="listTitle">동의한 후보 {agrees.length}명</h3>
+                    <ul className="list">
+                        {agrees.map(response => (
+                            <li key={`agree-${response.id}`} style={{padding: '10px 0'}}>
                                 <Response member={response.candidate}/>
                             </li>
                         ))}
